c14666d7d46db530542c6d09a3bb59b6533a76d0
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-10-17 14:21:34 -0500

    add login layout

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+using Mangaka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Mangaka.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LoginController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
