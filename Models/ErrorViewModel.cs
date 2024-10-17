e0b8db253dad11623cdb9d373ac3ee16ac44b1aa
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-10-17 14:10:30 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mangaka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
